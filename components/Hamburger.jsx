73095ca8edfe194fc26c23536772de487bc49405
kbagdanskas <karolis.bagdanskas@gmail.com>
2022-07-04 18:51:26 +0300

    bug fixed

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -11,11 +11,11 @@ const HamburgerMenu = () => (<div className='relative'>
 const HamburgerIcon = () => (<div className='p-1/2'><svg className="text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 6h16M4 12h16M4 18h16"></path></svg></div>)
 
 export const Links = () => (<>
-    <Link href="/"><a className='font-bold p-4'>Home</a></Link>
-    <Link href="/about"><a className='font-bold p-4'>Gerimai</a></Link>
-    <Link href="/pizzas"><a className='font-bold p-4'>Pica</a></Link>
-    <Link href="/more"><a className='font-bold p-4'>Priedai</a></Link>
-    <Link href="/contact"><a className='font-bold p-4'>Kontaktai</a></Link>
+    <Link href="/"><a className='font-bold listtext p-4'>Home</a></Link>
+    <Link href="/about"><a className='font-bold listtext p-4'>Gerimai</a></Link>
+    <Link href="/pizzas"><a className='font-bold listtext p-4'>Pica</a></Link>
+    <Link href="/more"><a className='font-bold listtext p-4'>Priedai</a></Link>
+    <Link href="/contact"><a className='font-bold listtext p-4'>Kontaktai</a></Link>
 </>)
 
 export default HamburgerMenu
